a69ef3efaad20b38715ffe843f32e63609093821
DevelopSoo <69490984+DevelopSoo@users.noreply.github.com>
2025-04-22 00:46:15 +0900

    test2

--- app/utils.test.js
+++ app/utils.test.js
@@ -3,3 +3,7 @@ import { sum } from "./utils";
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+test("subtracts 1 from 2 to equal 1", () => {
+  expect(subtract(2, 1)).toBe(1);
+});
